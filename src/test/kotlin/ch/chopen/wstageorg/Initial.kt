3910f65511b572003423cb20abb57fe7cff5460a
ollin <oliver.nautsch@gmail.com>
2017-04-11 14:51:02 +0200

    added unit-test infra

---
+++ src/test/kotlin/ch/chopen/wstageorg/Initial.kt
@@ -0,0 +1,14 @@
+package ch.chopen.wstageorg
+
+import io.kotlintest.specs.StringSpec
+
+/**
+ * unit test.
+ */
+class Initial : StringSpec() {
+    init {
+        "Application should work with default constructor" {
+            Application()
+        }
+    }
+}
